4d9fcfe174ee90e02bb49842f9ce4db4b0d31208
SysAdmin <potapovip@yandex.ru>
2022-07-13 17:19:46 +0500

    task 3 done

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Homework1 {
     public static void main(String[] args) {
         //Базовый уровень
@@ -7,6 +10,17 @@ public class Homework1 {
         char newLine = '\n';
         String text1 = hi.trim() + world + newLine;
         System.out.println(text1.repeat(3));
-
+        //Задача №2
+        int weith = 90;
+        double height = 1.87; //можно и через флоат, но тогда не верное кол-во цифр после запятой
+        System.out.printf("%.7f", weith / (height * height));
+        System.out.println();
+        //Задача №3
+        char[] alphabetArr = new char[]{'a','b','c','d','e'};
+        String alhabet = new String(alphabetArr);
+        System.out.println(alhabet);
+        alphabetArr[3] = 'h';
+        String alphabetH = new String(alphabetArr);
+        System.out.println(alphabetH);
     }
 }
