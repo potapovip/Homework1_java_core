c033b5f4a8ff9c89b3b4bde4ef4c70a52a357cc6
SysAdmin <potapovip@yandex.ru>
2022-07-13 11:15:46 +0500

    1 task done

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,2 +1,12 @@
 public class Homework1 {
+    public static void main(String[] args) {
+        //Базовый уровень
+        //Задача №1
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        String text1 = hi.trim() + world + newLine;
+        System.out.println(text1.repeat(3));
+
+    }
 }
