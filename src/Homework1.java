699f2533582cd9482befd4a0e5dd8fcfad631b06
potapovip <samurai1487@yandex.ru>
2022-07-13 19:24:00 +0500

    lvl 2 task 2 - done

--- src/Homework1.java
+++ src/Homework1.java
@@ -14,14 +14,34 @@ public class Homework1 {
         int weith = 90;
         double height = 1.87; //можно и через флоат, но тогда не верное кол-во цифр после запятой
         System.out.printf("%.7f", weith / (height * height));
-        System.out.println();
+        System.out.println('\n');
         //Задача №3
-        char[] alphabetArr = new char[]{'a','b','c','d','e'};
+        char[] alphabetArr = new char[]{'a', 'b', 'c', 'd', 'e'};
         String alhabet = new String(alphabetArr);
         System.out.println(alhabet);
         alphabetArr[3] = 'h';
         String alphabetH = new String(alphabetArr);
-        System.out.println(alphabetH);
+        System.out.println(alphabetH + '\n');
+        //Продвинутый уровень
+        //Задача №1
+        String simpleText = "234";
+        String somteText = "some_text";
+        System.out.println(Integer.parseInt(simpleText) + somteText.length());
         System.out.println();
+        //Задача №2
+        //Посчитать (a+b)^2 = ?, при a=3, b=5
+        int a = 3;
+        int b = 5;
+        int result = (a + b)*(a + b);
+        System.out.println(result);
+        //Задача №3
+        //Создать два массив чисел:
+        // 1,2,5,7,10
+        // 2,3,2,17,15
+        // Создать массив чисел, в котором будут: все числа из этих двух массивов,
+        // и результат умножения чисел с одинаковым порядковым номером
+
+
+
     }
 }
