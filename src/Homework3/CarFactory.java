78947973e9692adc1c4b33b8e6c7ccb41b678c79
potapovip <samurai1487@yandex.ru>
2022-07-28 00:27:17 +0500

    Homework 3: 2.1 progress

---
+++ src/Homework3/CarFactory.java
@@ -0,0 +1,11 @@
+package Homework3;
+
+public class CarFactory extends Avto {
+
+    public CarFactory(String carBrand) {
+        super(carBrand);
+    }
+    public static void createToyota (Toyota toyota){
+
+    }
+}
