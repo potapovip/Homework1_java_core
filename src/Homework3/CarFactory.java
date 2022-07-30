28b8a0e62d462df8c207dbf9461713100e5e1121
potapovip <samurai1487@yandex.ru>
2022-07-30 13:11:55 +0500

    Homework 3: 2.2 done

--- src/Homework3/CarFactory.java
+++ src/Homework3/CarFactory.java
@@ -1,26 +1,25 @@
 package Homework3;
 
 public class CarFactory {
-    private static int toyotaCount = 0;
-    private static int zhigulisCount = 0;
 
 
-    //    public static void createToyota(int carsQuantity) {
-//    Toyota[] toyotas = new Toyota[carsQuantity];
-//        for (int i = 0; i <= carsQuantity; i++) {
-//            toyotas[i] = new Toyota();
-//            toyotaCount++;
-//        }
-//    }
-//    public static void createZhigul (int carsQuantity){
-//        Zhiguli[] zhigulis = new Zhiguli[carsQuantity];
-//        for (int i = 0; i <= carsQuantity; i++) {
-//            zhigulis[i] = new Zhiguli();
-//            zhigulisCount++;
-//        }
-//    }
+        public static Toyota[] createToyota2 (int carsQuantity) {
+    Toyota[] toyotas = new Toyota[carsQuantity];
+        for (int i = 0; i < carsQuantity; i++) {
+            toyotas[i] = new Toyota();
+        }
+        return toyotas;
+    }
+    public static Zhiguli[] createZhiguli2 (int carsQuantity) {
+    Zhiguli[] zhigulis = new Zhiguli[carsQuantity];
+        for (int i = 0; i < carsQuantity; i++) {
+            zhigulis[i] = new Zhiguli();
+        }
+        return zhigulis;
+    }
+
     public static Toyota createToyota () {
-       return new Toyota();
+        return new Toyota();
     }
     public static Zhiguli createZhiguli () {
        return new Zhiguli();
