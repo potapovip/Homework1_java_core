f15e3b506e369f2021a2a7dabd2e5e090a2385fe
potapovip <samurai1487@yandex.ru>
2022-09-01 00:17:21 +0500

    Module 2 task2 in progress

---
+++ src/Module2/Homework1/Task2/Main.java
@@ -0,0 +1,12 @@
+package Module2.Homework1.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> integerMyList;
+        integerMyList = new MyList<>();
+        Integer[] integers = {1,2,3};
+
+
+    }
+
+}
--- src/Module2/Homework1/Task2/MyList.java
+++ src/Module2/Homework1/Task2/MyList.java
@@ -1,10 +1,28 @@
 package Module2.Homework1.Task2;
 
+import java.util.Arrays;
+
 public class MyList <T> {
     private T [] objects;
     int size;
-
+    //Создать метод add. Он будет добавлять элемент в массив object. Если в массиве уже 3 элемента есть, то нужно:
+    //1.Создать новый массив T[] с размерностью в два раза больше предыдущей.
+    //2.Скопировать со старого массива все значения в новый
+    //3.Присвоить экземпляру MyList новый массив.
     public MyList() {
         this.objects = (T[]) objects [3];
+    }
+    public  void add(T object){
+        for (int i = 0; i < objects.length ; i++) {
+                if (objects[i] == null){
+                    objects[i] = (T) object;
+                    break;
+                }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyList{" + "objects=" + Arrays.toString(objects) + '}';
     }
 }
