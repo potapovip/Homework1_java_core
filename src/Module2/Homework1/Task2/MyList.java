d5841a14e782916aa865f009c7159651dd8ae7ea
SysAdmin <potapovip@yandex.ru>
2022-08-31 17:39:07 +0500

    Module 2 task2 init commit

---
+++ src/Module2/Homework1/Task2/MyList.java
@@ -0,0 +1,10 @@
+package Module2.Homework1.Task2;
+
+public class MyList <T> {
+    private T [] objects;
+    int size;
+
+    public MyList() {
+        this.objects = (T[]) objects [3];
+    }
+}
