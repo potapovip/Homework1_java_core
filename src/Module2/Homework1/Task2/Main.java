0fd16d0293c17504ef2294bfba4904ba41f20e2f
SysAdmin <potapovip@yandex.ru>
2022-09-01 18:08:34 +0500

    Module 2 task2 завершил, но увы с подглядыванием в разбор.. Сложно пока, что для понимая даются касты.

--- src/Module2/Homework1/Task2/Main.java
+++ src/Module2/Homework1/Task2/Main.java
@@ -2,9 +2,16 @@ package Module2.Homework1.Task2;
 
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> integerMyList;
-        integerMyList = new MyList<>();
-        Integer[] integers = {1,2,3};
+        MyList<Integer> integerMyList = new MyList<>();
+
+        integerMyList.add(4);
+        integerMyList.add(5);
+        integerMyList.add(6);
+        integerMyList.add(7);
+        integerMyList.add(8);
+        integerMyList.add(9);
+        integerMyList.add(10);
+        System.out.println(integerMyList);
 
 
     }
--- src/Module2/Homework1/Task2/MyList.java
+++ src/Module2/Homework1/Task2/MyList.java
@@ -2,23 +2,30 @@ package Module2.Homework1.Task2;
 
 import java.util.Arrays;
 
-public class MyList <T> {
-    private T [] objects;
+public class MyList<T> {
+    private T[] objects = (T[]) new Object[3];
     int size;
+
     //Создать метод add. Он будет добавлять элемент в массив object. Если в массиве уже 3 элемента есть, то нужно:
     //1.Создать новый массив T[] с размерностью в два раза больше предыдущей.
     //2.Скопировать со старого массива все значения в новый
     //3.Присвоить экземпляру MyList новый массив.
-    public MyList() {
-        this.objects = (T[]) objects [3];
+
+
+    public T[] getObjects() {
+        return objects;
     }
-    public  void add(T object){
-        for (int i = 0; i < objects.length ; i++) {
-                if (objects[i] == null){
-                    objects[i] = (T) object;
-                    break;
-                }
+
+    public void add(T object) {
+        if (objects.length == size) {
+            T[] newObjects = (T[]) new Object[objects.length * 2];
+            for (int i = 0; i < objects.length; i++) {
+                newObjects[i] = objects[i];
+            }
+            objects = newObjects;
         }
+        objects[size] = object;
+        size++;
     }
 
     @Override
