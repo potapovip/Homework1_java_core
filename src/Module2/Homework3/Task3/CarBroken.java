d207af196070256ebc23ab0075fcb086e3582c10
SysAdmin <potapovip@yandex.ru>
2022-09-06 18:04:05 +0500

    Module 2 ДЗ 3: 3 задача в процессе

--- src/Module2/Homework3/Task3/CarBroken.java
+++ src/Module2/Homework3/Task3/CarBroken.java
@@ -1,4 +1,17 @@
 package Module2.Homework3.Task3;
 
 public class CarBroken {
+    public CarBroken() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
--- src/Module2/Homework3/Task3/Main.java
+++ src/Module2/Homework3/Task3/Main.java
@@ -1,9 +1,28 @@
 package Module2.Homework3.Task3;
 
+import java.util.HashMap;
+
 public class Main {
     //Задача №3
     //1. Создать класс CarBroken
     //2. Создать экземпляра класса HashMap<CarBroken, Integer>
     //3. Переопределить hashcode и equals так, чтобы при вставке любого CarBroken в HashMap,
     //всегда CarBroken попадал в один бакет.
+    public static void main(String[] args) {
+        CarBroken carBroken = new CarBroken();
+        CarBroken carBroken1 = new CarBroken();
+        CarBroken carBroken2 = new CarBroken();
+        CarBroken carBroken3 = new CarBroken();
+        CarBroken carBroken4 = new CarBroken();
+        CarBroken carBroken5 = new CarBroken();
+        CarBroken carBroken6 = new CarBroken();
+        HashMap<CarBroken, Integer> carBrokenIntegerHashMap = new HashMap<>();
+        carBrokenIntegerHashMap.put(carBroken,1);
+        carBrokenIntegerHashMap.put(carBroken2,1);
+        carBrokenIntegerHashMap.put(carBroken3,1);
+        carBrokenIntegerHashMap.put(carBroken4,1);
+        carBrokenIntegerHashMap.put(carBroken5,1);
+        carBrokenIntegerHashMap.put(carBroken6,1);
+        System.out.println(carBrokenIntegerHashMap);
+    }
 }
